982d32fc4a61f5a981a45d8bc6c891147ed0f6b7
Thomas Erlang <thomas@erlang.dk>
2019-06-21 19:37:03 +0200

    Fixed not importing logging.handlers

--- storitch/logger.py
+++ storitch/logger.py
@@ -1,4 +1,4 @@
-import logging, os
+import logging, logging.handlers, os
 from storitch import config
 
 class logger(object):
@@ -7,6 +7,7 @@ class logger(object):
     def set_logger(cls, filename, to_sentry=False, fmt='[%(levelname)s %(asctime)s.%(msecs)d %(module)s:%(lineno)d]: %(message)s'):
         logger = logging.getLogger()
         logger.setLevel(getattr(logging, config['logging']['level'].upper()))
+
         format_ = logging.Formatter(
             fmt, 
             datefmt='%Y-%m-%d %H:%M:%S'
