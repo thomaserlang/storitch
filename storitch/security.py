4a790e90b5690ea6aca990402726c4785a8b5dfa
Thomas Erlang <thomas@erlang.dk>
2023-06-08 20:50:40 +0200

    Fixed api key validation

--- storitch/security.py
+++ storitch/security.py
@@ -3,9 +3,8 @@ from fastapi.security.api_key import APIKeyHeader
 from fastapi import Header, Security, HTTPException
 from . import config
 
-api_key_header = APIKeyHeader(name="Authorization", auto_error=False, description="API Key")
 
 async def validate_api_key(authorization: Annotated[str, Header(description='API Key')]):
-    if api_key_header in config.api_keys:
-        return api_key_header
+    if authorization in config.api_keys:
+        return authorization
     raise HTTPException(status_code=403, detail="Invalid API Key")
