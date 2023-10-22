c8064ac47c87f9ffa0d53445dfc9dde5ee0da843
Thomas Erlang <thomas@erlang.dk>
2023-10-22 14:01:49 +0200

    Test dcm

--- tests/upload.py
+++ tests/upload.py
@@ -1,12 +1,10 @@
-import requests, json
-from storitch import config
-
+import requests
 
 def upload_multipart():
     r = requests.post(
-        f'http://127.0.0.1:{config.port}/store',
+        f'http://127.0.0.1:3000/store',
         files={'file': open('./tests/test1.txt', 'rb')},
-        headers={'Authorization': config.api_keys[0]},
+        headers={'Authorization': 'test'},
     )
     assert r.status_code == 201, r.content
     d = r.json()
@@ -21,14 +19,14 @@ def upload_session():
         while True:
             d = f.read(5)
             args = dict(
-                url=f'http://127.0.0.1:{config.port}/store/session',
+                url=f'http://127.0.0.1:3000/store/session',
                 data=d,
                 headers={
                     'Content-Type': 'application/octet-stream',
                     'X-Session': session,
                     'X-Filename': 'testæøå.txt',
                     'X-Finished': 'false' if d else 'true',
-                    'Authorization': config.api_keys[0],
+                    'Authorization': 'test',
                 },
             )
             r = requests.post(**args) if start else requests.patch(**args)
@@ -46,29 +44,48 @@ def upload_session():
 
 def thumbnail():
     r = requests.post(
-        f'http://127.0.0.1:{config.port}/store',
+        f'http://127.0.0.1:3000/store',
         files={'file': open('./tests/test.png', 'rb')},
-        headers={'Authorization': config.api_keys[0]},
+        headers={'Authorization': 'test'},
     )
     assert r.status_code == 201, r.content
     d = r.json()
     assert d[0]['hash'] == '1171aad9f52efe4f577ccabec4aaeb063e28a80978f3853721381bca2b5fe501'
-    assert d[0]['type'] == 'image'
+    assert d[0]['type'] == 'image', d[0]['type']
     assert d[0]['width'] == 5
     assert d[0]['height'] == 5
 
     r = requests.get(
-        f'http://127.0.0.1:{config.port}/{d[0]["file_id"]}@.jpg',
+        f'http://127.0.0.1:3000/{d[0]["file_id"]}@.jpg',
     )
     assert r.status_code == 200, r.content
 
     r = requests.get(
-        f'http://127.0.0.1:{config.port}/{d[0]["file_id"]}',
+        f'http://127.0.0.1:3000/{d[0]["file_id"]}@.ps',
+    )
+    assert r.status_code == 500, r.content
+
+
+def dcm_thumbnail():
+    r = requests.post(
+        f'http://127.0.0.1:3000/store',
+        files={'file': open('./tests/0002.DCM', 'rb')},
+        headers={'Authorization': 'test'},
+    )
+    assert r.status_code == 201, r.content
+    d = r.json()
+    assert d[0]['hash'] == 'ee1fcf71fecb6a8aee3b1219388cc7ded40a804056a7647674ba1a77b797ae8e', d[0]['hash']
+    assert d[0]['type'] == 'image', d[0]['type']
+    assert d[0]['width'] == 512, d[0]['width']
+    assert d[0]['height'] == 512, d[0]['height']
+
+    r = requests.get(
+        f'http://127.0.0.1:3000/{d[0]["file_id"]}@.jpg',
     )
     assert r.status_code == 200, r.content
-
 
 if __name__ == '__main__':
     upload_multipart() 
     upload_session()
     thumbnail()
+    dcm_thumbnail()
