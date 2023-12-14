7dc1405eee6b37103c8c0f691eb81e52603c45cc
Thomas Erlang <thomas@erlang.dk>
2023-12-14 14:08:38 +0100

    Support range requests response

--- storitch/routes/download.py
+++ storitch/routes/download.py
@@ -1,8 +1,9 @@
 import asyncio
 import re, os, logging
 from aiofiles import os as aioos
-from fastapi import APIRouter, HTTPException
-from fastapi.responses import FileResponse
+import anyio
+from fastapi import APIRouter, HTTPException, Request, status
+from fastapi.responses import FileResponse, StreamingResponse
 from mimetypes import guess_type
 from pydantic import constr
 from storitch import config
@@ -38,8 +39,11 @@ it to a PNG file.
 '''
 @router.get('/{file_id}', response_class=FileResponse, description=description)
 @router.get('/{file_id}/{filename}', response_class=FileResponse, description=description)
+@router.head('/{file_id}', description=description)
+@router.head('/{file_id}/{filename}', description=description)
 async def download(
     file_id: constr(pattern=r'[a-zA-Z0-9-]+(@[a-zA-Z0-9_-.]+)?'),
+    request: Request,
     filename: str = None,
 ):
     path = permanent_store.get_file_path(file_id)
@@ -49,11 +53,25 @@ async def download(
             raise HTTPException(status_code=500, detail='Failed to convert file.')
         path = converted
     try:
-        stat = await aioos.stat(path)
-        return FileResponse(
+        stat_result = await aioos.stat(path)
+        media_type = guess_type(filename or file_id)[0] or "application/octet-stream"
+
+        if request.method == 'HEAD':
+            return FileResponse(
+                path,
+                status_code=200, 
+                media_type=media_type, 
+                filename=filename,
+                method='HEAD',
+                stat_result=stat_result,
+            )
+        
+        return range_requests_response(
+            request=request,
             path=path,
-            stat_result=stat,
-            media_type=guess_type(filename or file_id)[0] or 'application/octet-stream',
+            filename=filename or file_id,
+            media_type=media_type,
+            stat_result=stat_result,
         )
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail='Not found')
@@ -122,6 +140,76 @@ def _get_size(arguments: str, convert_args: list[str]):
             convert_args.append(f'x{size_match.group(2)}')
             return f'SY{size_match.group(2)}'
     return ''
+
+
+def range_requests_response(
+    request: Request, 
+    path: str, 
+    filename: str,
+    media_type: str,
+    stat_result: os.stat_result,    
+):
+    """Returns StreamingResponse using Range Requests of a given file"""
+
+    file_size = stat_result.st_size
+    range_header = request.headers.get("range")
+
+    f = FileResponse(
+        path=path, 
+        stat_result=stat_result, 
+        filename=filename,
+        media_type=media_type,
+    )
+    headers = f.headers
+    headers['accept-ranges'] = 'bytes'
+    headers['content-encoding'] = 'identity'
+    headers['access-control-expose-headers'] = (
+        'content-type, accept-ranges, content-length, '
+        'content-range, content-encoding'
+    )
+    start = 0
+    end = file_size - 1
+    status_code = status.HTTP_200_OK
+
+    if range_header is not None:
+        start, end = _get_range_header(range_header, file_size)
+        size = end - start + 1
+        headers["content-length"] = str(size)
+        headers["content-range"] = f"bytes {start}-{end}/{file_size}"
+        status_code = status.HTTP_206_PARTIAL_CONTENT
+
+    return StreamingResponse(
+        _send_bytes(path, start, end),
+        headers=headers,
+        status_code=status_code,
+    )
+
+
+def _get_range_header(range_header: str, file_size: int) -> tuple[int, int]:
+    def _invalid_range():
+        return HTTPException(
+            status.HTTP_416_REQUESTED_RANGE_NOT_SATISFIABLE,
+            detail=f"Invalid request range (Range:{range_header!r})",
+        )
+
+    try:
+        h = range_header.replace("bytes=", "").split("-")
+        start = int(h[0]) if h[0] != "" else 0
+        end = int(h[1]) if h[1] != "" else file_size - 1
+    except ValueError:
+        raise _invalid_range()
+
+    if start > end or start < 0 or end > file_size - 1:
+        raise _invalid_range()
+    return start, end
+
+
+async def _send_bytes(path: str, start: int, end: int):
+    async with await anyio.open_file(path, mode="rb") as f:
+        await f.seek(start)
+        while (pos := await f.tell()) <= end:
+            read_size = min(FileResponse.chunk_size, end + 1 - pos)
+            yield await f.read(read_size)
 
 
 def is_allowed_size(size: int):
