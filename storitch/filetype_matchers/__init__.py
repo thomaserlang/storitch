6c0ebed7448dd1a03071357c8930cdc834dd0f2b
Thomas Erlang <thomas@erlang.dk>
2024-10-27 19:43:35 +0100

    Removed debug print

--- storitch/filetype_matchers/__init__.py
+++ storitch/filetype_matchers/__init__.py
@@ -1,6 +1,5 @@
-from filetype import Type, add_type
+from filetype import add_type
 
 from . import m3shape_dcm
 
-print(isinstance(m3shape_dcm.M3shapeDCM, Type))
 add_type(m3shape_dcm.M3shapeDCM())
