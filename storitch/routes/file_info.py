29ec87fd45931de2a26c6f818bddd274ee03954a
Thomas Erlang <thomas@erlang.dk>
2024-11-01 14:17:08 +0100

    Fix not capturing exceptions for file info endpoint

--- storitch/routes/file_info.py
+++ storitch/routes/file_info.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from pydicom.errors import InvalidDicomError
 
 from storitch import store_file
 from storitch.identify_file import get_file_info
@@ -27,6 +28,19 @@ async def get_file_info_route(
     file_id: str,
     filename: str | None = None,
 ):
-    return await get_file_info(
-        file_path=store_file.get_file_path(file_id=file_id), filename=filename or ''
-    )
+    try:
+        return await get_file_info(
+            file_path=store_file.get_file_path(file_id=file_id), filename=filename or ''
+        )
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail='Not found')
+    except OSError:
+        raise HTTPException(status_code=500, detail='Failed to open file')
+    except ValueError:
+        raise HTTPException(
+            status_code=400, detail='Invalid DICOM file, does not look like an image'
+        )
+    except InvalidDicomError:
+        raise HTTPException(
+            status_code=400, detail='Invalid DICOM file, could not read file'
+        )
